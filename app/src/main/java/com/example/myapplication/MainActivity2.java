ff1f8e5017a57751ee9e40752ceaa4985ec68492
Atakan Him <atakanhim@gmail.com>
2021-05-24 18:46:53 +0300

    galibaoldu
    galibaoldu

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,105 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    SQLiteDatabase database;
+    ArrayList<Place> placeList = new ArrayList<>();
+    ListView listView;
+    CustomAdapter customAdapter;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_place,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_place) {
+            Intent intent = new Intent(this,MapsActivity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        listView = findViewById(R.id.listView);
+        getData();
+    }
+
+
+    public void getData() {
+        customAdapter = new CustomAdapter(this,placeList);
+
+        try {
+
+            database = this.openOrCreateDatabase("Places",MODE_PRIVATE,null);
+            Cursor cursor = database.rawQuery("SELECT * FROM places",null);
+
+            int nameIx = cursor.getColumnIndex("name");
+            int latitudeIx = cursor.getColumnIndex("latitude");
+            int longitudeIx = cursor.getColumnIndex("longitude");
+
+            while (cursor.moveToNext()) {
+
+                String nameFromDatabase = cursor.getString(nameIx);
+                String latitudeFromDatabase = cursor.getString(latitudeIx);
+                String longitudeFromDatabase = cursor.getString(longitudeIx);
+
+                Double latitude = Double.parseDouble(latitudeFromDatabase);
+                Double longitude = Double.parseDouble(longitudeFromDatabase);
+
+                Place place = new Place(nameFromDatabase,latitude,longitude);
+
+                System.out.println(place.name);
+
+                placeList.add(place);
+
+            }
+            customAdapter.notifyDataSetChanged();
+            cursor.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity2.this,MapsActivity.class);
+                intent.putExtra("info","old");
+                intent.putExtra("place",placeList.get(i));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/Place.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import java.io.Serializable;
+
+public class Place implements Serializable {
+
+     public String name;
+     public Double latitude;
+     public Double longitude;
+
+    public Place(String name, Double latitude, Double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+}
